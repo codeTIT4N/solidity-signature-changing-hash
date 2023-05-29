fdccfb70a13b10f7d482d79e1d9259c2ab5b31c1
codeTIT4N <lokeshkumar636@yahoo.com>
2023-05-29 22:06:45 +0530

    more tests

--- test/SignatureChangingHash.test.ts
+++ test/SignatureChangingHash.test.ts
@@ -57,5 +57,85 @@ describe("SignatureChangingHash contract tests", function () {
       expect(await contract.chainId()).to.equal(network.config.chainId);
     });
   });
-  describe("Contract tests", function () {}); // @todo
+  describe("Functionality tests", function () {
+    it("getHashTimestamp(): Should return the same value as the reference timestamp in the beginning", async function () {
+      const { contract } = await loadFixture(deployContract);
+      expect(await contract.getHashTimestamp()).to.equal(
+        await contract.referenceTimestamp()
+      );
+    });
+    it("getHashTimestamp(): Value should not change before 2 minutes", async function () {
+      const { contract } = await loadFixture(deployContract);
+      // increase time by 1 minute
+      await network.provider.send("evm_increaseTime", [60]);
+      await network.provider.send("evm_mine");
+      expect(await contract.getHashTimestamp()).to.equal(
+        await contract.referenceTimestamp()
+      );
+      // increase time by 59 seconds
+      await network.provider.send("evm_increaseTime", [59]);
+      await network.provider.send("evm_mine");
+      expect(await contract.getHashTimestamp()).to.equal(
+        await contract.referenceTimestamp()
+      );
+    });
+    it("getHashTimestamp(): Value should increase after every 2 minutes", async function () {
+      const { contract } = await loadFixture(deployContract);
+      // increase time by 2 minutes
+      await network.provider.send("evm_increaseTime", [120]);
+      await network.provider.send("evm_mine");
+      expect(await contract.getHashTimestamp()).to.not.equal(
+        await contract.referenceTimestamp()
+      );
+      expect(await contract.getHashTimestamp()).to.above(
+        await contract.referenceTimestamp()
+      );
+      // again increase time by 2 minutes
+      await network.provider.send("evm_increaseTime", [120]);
+      await network.provider.send("evm_mine");
+      expect(await contract.getHashTimestamp()).to.not.equal(
+        await contract.referenceTimestamp()
+      );
+      expect(await contract.getHashTimestamp()).to.above(
+        await contract.referenceTimestamp()
+      );
+    });
+    it("getHash(): Should create correct hash according to values from constructor", async function () {
+      const { contract } = await loadFixture(deployContract);
+      const hash = await contract.getHash();
+      const hash2 = ethers.utils.solidityKeccak256(
+        ["address", "uint256", "uint256", "uint256"],
+        [
+          contract.address,
+          await contract.chainId(),
+          await contract.nonce(),
+          await contract.referenceTimestamp(),
+        ]
+      );
+      expect(hash).to.equal(hash2);
+    });
+    it("getHash(): Hash should not change before 2 minutes", async function () {
+      const { contract } = await loadFixture(deployContract);
+      const hash = await contract.getHash();
+      // increase time by 1 minutes
+      await network.provider.send("evm_increaseTime", [60]);
+      await network.provider.send("evm_mine");
+      const hash2 = await contract.getHash();
+      expect(hash).to.equal(hash2);
+      // increase time by 59 seconds
+      await network.provider.send("evm_increaseTime", [59]);
+      await network.provider.send("evm_mine");
+      const hash3 = await contract.getHash();
+      expect(hash).to.equal(hash3);
+    });
+    it("getHash(): Hash should change after 2 minutes", async function () {
+      const { contract } = await loadFixture(deployContract);
+      const hash = await contract.getHash();
+      // increase time by 2 minutes
+      await network.provider.send("evm_increaseTime", [120]);
+      await network.provider.send("evm_mine");
+      const hash2 = await contract.getHash();
+      expect(hash).to.not.equal(hash2);
+    });
+  });
 });
